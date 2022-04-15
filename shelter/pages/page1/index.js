ddca77a953be4e7790ee00363ca4b1fabbee466b
Svetlana <lanavladi1605gmail.com>
2022-04-15 20:52:48 +0500

    fix: bg color (stretch the background on two block header and start screen)

--- shelter/pages/page1/index.js
+++ shelter/pages/page1/index.js
@@ -1,6 +1,5 @@
 
-console.log(`**Если будут какие-то вопросы по работе, прошу связаться со мной в дискорде LanaVladi#2712 
-или телеграмме @lanainthesky
+console.log(`Если будут какие-то вопросы по работе, прошу связаться со мной в дискорде LanaVladi#2712 или телеграмме @lanainthesky
 
 
 Self-check:
@@ -8,7 +7,4 @@ Self-check:
 2. Screenshoot: ![image](https://user-images.githubusercontent.com/93492668/162847183-f8d11248-88ae-44da-8ad7-c6b54608298d.png)
 3. Deploy: https://rolling-scopes-school.github.io/lanavladi-JSFE2022Q1/shelter/pages/page1/
 4. Done 12.04.2022 / deadline 12.04.2022 
-5. Score: 100/ 100
-
-При самостоятельной проверке работы почему-то не сразу заработал ховер у кнопки с расчетным счетом в разделе DONATION, 
-после нажатия на кнопку ховер стал появляться. Пожалуйста попробуйте несколько раз и убедитесь, что ховер есть =))) `);
+5. Score: 100/100`);
