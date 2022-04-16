2c78cc4ff5d634a26e4d267646370873662841ac
Svetlana <lanavladi1605gmail.com>
2022-04-16 15:53:23 +0500

    feat: add styles for header & start screen for 768px

--- shelter/pages/page1/index.js
+++ shelter/pages/page1/index.js
@@ -6,5 +6,5 @@ Self-check:
 1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/stage1/stream1/shelter/shelter-ru.md
 2. Screenshoot: ![image](https://user-images.githubusercontent.com/93492668/162847183-f8d11248-88ae-44da-8ad7-c6b54608298d.png)
 3. Deploy: https://rolling-scopes-school.github.io/lanavladi-JSFE2022Q1/shelter/pages/page1/
-4. Done 12.04.2022 / deadline 12.04.2022 
-5. Score: 100/100`);
+4. Done 12.04.2022 / deadline 12.04.2022
+5. Score: 100 /100`);
