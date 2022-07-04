5160343eef51608ec21f0698bc98ab7779234f9b
Svetlana <lanavladi05@gmail.com>
2022-07-05 00:34:30 +0500

    fix: src/components/view/sources/sources.ts

--- migration-to-typeScript/src/components/view/sources/sources.ts
+++ migration-to-typeScript/src/components/view/sources/sources.ts
@@ -1,20 +1,23 @@
 import './sources.css';
+import { IDataSources } from '../../../types';
 
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+  public draw(data: Array<IDataSources>): void {
+        const fragment = document.createDocumentFragment()  as DocumentFragment;
+        const sourceItemTemp = document.querySelector('#sourceItemTemp')  as HTMLTemplateElement | null;
+            if (sourceItemTemp === null) {
+                throw new Error('Could not find element.');
+            }
+        data.forEach((item: IDataSources) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
-
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+            (sourceClone.querySelector('.source__item-name') as HTMLElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLElement).setAttribute('data-source-id', item.id);
 
             fragment.append(sourceClone);
         });
 
-        document.querySelector('.sources').append(fragment);
+        (document.querySelector('.sources') as HTMLElement).append(fragment);
     }
 }
 
