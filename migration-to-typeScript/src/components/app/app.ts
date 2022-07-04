598a533e4cac7cdf5e21bb2127aad7ecc98349a2
Svetlana <lanavladi05@gmail.com>
2022-07-05 00:35:21 +0500

    fix: src/components/app/app.ts

--- migration-to-typeScript/src/components/app/app.ts
+++ migration-to-typeScript/src/components/app/app.ts
@@ -1,17 +1,24 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { IDataDrawSources, IDataDrawNews } from '../../types';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
-    start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+    start(): void {
+          (document
+            .querySelector('.sources') as HTMLElement)
+            .addEventListener('click', (e: Event) => this.controller.getNews(e, (data: IDataDrawNews): void =>
+              this.view.drawNews(data)));
+
+        this.controller.getSources((data: IDataDrawSources): void =>
+         this.view.drawSources(data));
+
     }
 }
 
