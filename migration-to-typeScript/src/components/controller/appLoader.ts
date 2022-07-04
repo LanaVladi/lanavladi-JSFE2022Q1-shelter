0876974b6d5d5468c130331734e0c80e2ed380d0
Svetlana <lanavladi05@gmail.com>
2022-07-05 01:14:35 +0500

    fix: URL in src/components/controller/appLoader.ts

--- migration-to-typeScript/src/components/controller/appLoader.ts
+++ migration-to-typeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'a32fe19ad6754b2a8ee089da70f56554',  // получите свой ключ https://newsapi.org/
         });
     }
