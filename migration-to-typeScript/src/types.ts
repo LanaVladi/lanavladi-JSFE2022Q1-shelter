049882344d655a055827600651a571366a22e460
Svetlana <lanavladi05@gmail.com>
2022-07-05 00:36:17 +0500

    feat: add src/types.ts

---
+++ migration-to-typeScript/src/types.ts
@@ -0,0 +1,45 @@
+export interface ILoader {
+readonly baseLink: string;
+options: object;
+}
+
+export type Options = Record<string, string> | undefined;
+
+export type Callback<T> = (data: T) => void;
+
+// sources.ts & appView.ts
+export interface IDataSources {
+category: string;
+country: string;
+description: string;
+id: string;
+language: string;
+name: string;
+url: string;
+}
+
+export interface IDataDrawSources {
+status: string;
+sources: Array<IDataSources>;
+}
+
+// news.ts & appView.ts
+export interface IDataNewsArticles {
+author: string;
+content: string;
+description: string;
+publishedAt: string;
+source: {
+    id: string;
+    name: string;
+};
+title: string;
+url: string;
+urlToImage: string;
+}
+
+export interface IDataDrawNews {
+status: string;
+totalResults: number;
+articles: Array<IDataNewsArticles>;
+}
