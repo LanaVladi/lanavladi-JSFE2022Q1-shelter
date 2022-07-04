2edc014eb3f382998f2df6ad2cf3cf5d5875021a
Svetlana <lanavladi05@gmail.com>
2022-07-05 00:15:33 +0500

    fix: src/components/controller/appLoader.js

--- migration-to-typeScript/src/components/controller/appLoader.ts
+++ migration-to-typeScript/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'a32fe19ad6754b2a8ee089da70f56554', // получите свой ключ https://newsapi.org/
+            apiKey: 'a32fe19ad6754b2a8ee089da70f56554',  // получите свой ключ https://newsapi.org/
         });
     }
 }
