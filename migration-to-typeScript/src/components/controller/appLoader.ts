ca375a5a9cc3604cfc1d54feb9ea13f4efd4c5d9
Svetlana <lanavladi05@gmail.com>
2022-07-08 16:59:27 +0500

    refactor: add const instead of apiKey & URL src/components/controller/appLoader.ts

--- migration-to-typeScript/src/components/controller/appLoader.ts
+++ migration-to-typeScript/src/components/controller/appLoader.ts
@@ -1,9 +1,10 @@
+import { apiKey, baseLink } from '../../utils';
 import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
-            apiKey: 'a32fe19ad6754b2a8ee089da70f56554',  // получите свой ключ https://newsapi.org/
+        super(baseLink, {
+            apiKey: apiKey,
         });
     }
 }
