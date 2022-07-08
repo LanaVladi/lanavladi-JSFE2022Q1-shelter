82d0f3b27ad713fd7387719612e75bb71b663b55
Svetlana <lanavladi05@gmail.com>
2022-07-08 16:44:27 +0500

    refactor: add string enums & change tabulation src/types.ts

--- migration-to-typeScript/src/types.ts
+++ migration-to-typeScript/src/types.ts
@@ -1,45 +1,50 @@
 export interface ILoader {
-readonly baseLink: string;
-options: object;
+  readonly baseLink: string;
+  options: object;
 }
 
 export type Options = Record<string, string> | undefined;
 
 export type Callback<T> = (data: T) => void;
 
+export enum Endpoint {
+  sources = 'sources',
+  everything = 'everything',
+}
+
 // sources.ts & appView.ts
 export interface IDataSources {
-category: string;
-country: string;
-description: string;
-id: string;
-language: string;
-name: string;
-url: string;
+  category: string;
+  country: string;
+  description: string;
+  id: string;
+  language: string;
+  name: string;
+  url: string;
 }
 
 export interface IDataDrawSources {
-status: string;
-sources: Array<IDataSources>;
+  status: string;
+  sources: Array<IDataSources>;
 }
 
 // news.ts & appView.ts
 export interface IDataNewsArticles {
-author: string;
-content: string;
-description: string;
-publishedAt: string;
-source: {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: {
     id: string;
     name: string;
-};
-title: string;
-url: string;
-urlToImage: string;
+  };
+  title: string;
+  url: string;
+  urlToImage: string;
 }
 
 export interface IDataDrawNews {
-status: string;
-totalResults: number;
-articles: Array<IDataNewsArticles>;
+  status: string;
+  totalResults: number;
+  articles: Array<IDataNewsArticles>;
 }
