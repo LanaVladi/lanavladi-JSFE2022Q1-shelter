2007fb1d5f4e052def90b9823da7d892a90245af
Svetlana <lanavladi05@gmail.com>
2022-07-11 14:42:54 +0500

    refactor:  remove types from const in utils.ts

--- migration-to-typeScript/src/utils.ts
+++ migration-to-typeScript/src/utils.ts
@@ -1,4 +1,4 @@
-export const apiKey: string = 'a32fe19ad6754b2a8ee089da70f56554';
+export const apiKey = 'a32fe19ad6754b2a8ee089da70f56554';
 
-export const baseLink: string = 'https://nodenews.herokuapp.com/';
+export const baseLink = 'https://nodenews.herokuapp.com/';
 
