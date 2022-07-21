e61114ff49fbd4243e601238340542992378846b
Svetlana <lanavladi05@gmail.com>
2022-07-21 19:06:06 +0500

    feat: add types.ts

---
+++ online-store/src/types.ts
@@ -0,0 +1,11 @@
+export interface IBooksList {
+    id: string;
+    bookName: string;
+    author: string;
+    price: string;
+    publishingHouse: string;
+    quantityInStock: string;
+    image: string;
+    publicationYear: string;
+    isPopular: string;
+}
