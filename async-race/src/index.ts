a28cdd53bcb88fac41558b284be685584139bece
Svetlana <lanavladi1605gmail.com>
2022-08-11 00:54:16 +0500

    feat: add index.ts to src

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,1 +1,7 @@
 import './style.css';
+import { renderViews } from './components/ui';
+import { createCarsBtn, generateCars } from './components/garage/garageBtn';
+
+generateCars();
+createCarsBtn();
+renderViews();
